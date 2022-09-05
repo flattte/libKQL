9f885f4c2fa40ed35234747d04bf9e862bb6e376
flattte <79518913+flattte@users.noreply.github.com>
2022-09-05 10:36:13 +0200

    Update basic_enum_control.hs

--- th_queries/basic_enum_control.hs
+++ th_queries/basic_enum_control.hs
@@ -1,3 +1,4 @@
 DeviceProcessEvents
 | where InitiatingProcessFileName has_any ("powershell.exe", "cmd.exe", "wscript.exe", "cscript.exe")
 | where ProcessCommandLine has_any ("Get-ADDomain", "Get-SMBShare", "Get-ADDomainController", "Get-ADUser", "Get-AdComputer", "Get-ADGroup", "Get-ADGroupMember", "Get-ADPrincipalGroupMembership")
+
